87c2451ed1b1aa43deb5404c49668c4d649f41c5
RobicaK <67951380+RobicaK@users.noreply.github.com>
2020-12-15 18:46:43 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let isIsland = false;
+let language = 'ROMANA';
+const country = 'Romania';
+let population = ' 18 mil';
+
+language = ' ENGLEZA';
+
+
+console.log(language);
+console.log(isIsland);
+console.log(population);
+console.log(country);
+
+
